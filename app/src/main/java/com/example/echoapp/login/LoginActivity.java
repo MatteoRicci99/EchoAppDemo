3337062f8e7e7bb2195a3d3e2d752b99a9af809d
MatteoRicci99 <matteoricci975@gmail.com>
2021-07-04 17:30:14 +0200

    matteo

--- app/src/main/java/com/example/echoapp/login/LoginActivity.java
+++ app/src/main/java/com/example/echoapp/login/LoginActivity.java
@@ -1,4 +1,25 @@
 package com.example.echoapp.login;
 
-public class LoginActivity {
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.echoapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText Email;
+    private EditText Password;
+    private Button Accedi;
+    private Button Registrati;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+
+    }
+
 }
